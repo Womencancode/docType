9a9c9597e4c97f596dedaea98db591d85e59f4c6
devnrj07 <niraj.mukta123@gmail.com>
2020-09-19 20:37:02 +0530

    Updates

---
+++ precache-manifest.eed4f09fd4ea8fff07cc72bc0c78074d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c7c72e5e63109601f668234e73d73e",
+    "url": "/docType/index.html"
+  },
+  {
+    "revision": "56482d3afe7a4fb604b2",
+    "url": "/docType/static/css/main.6f448de2.chunk.css"
+  },
+  {
+    "revision": "44ed3f0b4fa6dbc8b8ff",
+    "url": "/docType/static/js/2.c8120dd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/docType/static/js/2.c8120dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56482d3afe7a4fb604b2",
+    "url": "/docType/static/js/main.efe7f13a.chunk.js"
+  },
+  {
+    "revision": "c8c6b3d460d8a0603ff6",
+    "url": "/docType/static/js/runtime-main.5885c091.js"
+  }
+]);
